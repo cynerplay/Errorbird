d94b040562536f712e38d4ebaddaec2f1f534489
cynerplay <150257505+cynerplay@users.noreply.github.com>
2025-04-05 17:42:48 +0300

    Add files via upload

---
+++ FlappyBird/01233210.py
@@ -0,0 +1,2 @@
+print("pygame 2.6.1 (SDL 2.28.4, Python 3.7.0)")
+print("Hello from the pygame community. https://www.pygame.org/contribute.html")
---
+++ FlappyBird/123321.py
@@ -0,0 +1,2 @@
+print("pygame 2.1.2 (SDL 2.0.18, Python 3.7.0)")
+print("Hello from the pygame community. https://www.pygame.org/contribute.html")
